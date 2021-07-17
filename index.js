ad05cf9b79604751be547977b9ba3c077e278043
OlegTch <Olgard984@gmail.com>
2021-07-17 17:37:00 +0300

    Добавляет цикл в банкомат

--- index.js
+++ index.js
@@ -8,9 +8,9 @@
 
 // import './js/task-1-1.js';
 // import './js/task-1-2.js';
-// import './js/task-1-2-atm.js';
+import './js/task-1-2-atm.js';
 // import './js/task-1-3.js';
-import './js/task-1-3-numbers.js';
+// import './js/task-1-3-numbers.js';
 // import './js/task-1-4.js';
 // import './js/task-1-5.js';
 // import './js/task-1-6.js';
